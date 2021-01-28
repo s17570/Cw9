dc2a1dfb8a477d9b6775ec5eacefbda7ead93eb8
s17570 <s17570@pjwstk.edu.pl>
2021-01-28 23:04:08 +0100

    Zadanie 9.1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            //d.Przyklad1();
+        }
+    }
+}
